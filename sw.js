1eaed238376bcd658c1ba28801e7dabef708d79e
smorawicki-ui <smorawicki@gmail.com>
2026-09-03 11:36:47 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,36 @@
-self.addEventListener('fetch', function(event) {});
+const CACHE_NAME = 'morachord-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
